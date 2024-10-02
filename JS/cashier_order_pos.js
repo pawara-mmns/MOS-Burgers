4dbbb886e30e6cc0d2cd75bd86acb5b96d793572
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2024-10-02 15:36:46 +0530

    Updated menu data with new items and prices

--- JS/cashier_order_pos.js
+++ JS/cashier_order_pos.js
@@ -2,15 +2,24 @@ const menuData = [
     { id: 1, name: "Big Mac", price: 500.00, image: "ITEM_IMG/DC_202302_0005-999_BigMac_1564x1564-1_nutrition-calculator-tile.jpeg" },
     { id: 2, name: "Veggie Burger", price: 450.00, image: "ITEM_IMG/DC_202302_0004-999_DoubleCheeseburger_Alt_1564x1564-1_nutrition-calculator-tile.jpeg" },
     { id: 3, name: "Chicken Burger", price: 550.00, image: "ITEM_IMG/DC_202309_4282_QuarterPounderCheeseDeluxe_Shredded_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 4, name: "French Fries", price: 200.00, image: "ITEM_IMG/DC_202002_6050_SmallFrenchFries_Standing_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 5, name: "Soft Drink", price: 150.00, image: "ITEM_IMG/DC_202402_0521_MediumCoke_ContourGlassv1_1564x1564_nutrition-calculator-tile.jpeg" },
-    { id: 6, name: "Milkshake", price: 300.00, image: "ITEM_IMG/DC_201907_1509_MediumChocolateShake_Glass_A1_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 7, name: "Spicy Burger", price: 250.00, image: "ITEM_IMG/DC_202012_0116_SpicyCrispyChicken_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 8, name: "Crispy Chicken Burger", price: 850.00, image: "ITEM_IMG/DC_202012_0383_CrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 9, name: "Spicy Double Chicken Burger", price: 1050.00, image: "ITEM_IMG/DC_202104_0100_DeluxeSpicyCrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 10, name: "Sprite", price: 280.00, image: "ITEM_IMG/DC_202212_0721_MediumSprite_Glass_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 11, name: "MOS Water", price: 199.00, image: "ITEM_IMG/DC_202402_5474_DasaniBottledWater_1564x1564-1_nutrition-calculator-tile.jpeg" },
-    { id: 12, name: "Latte", price: 300.00, image: "ITEM_IMG/DC_201906_7659_MediumIcedMocha_Glass_A1_832x472_nutrition-calculator-tile.jpeg" }
+    { id: 4, name: "Crispy Chicken Burger", price: 850.00, image: "ITEM_IMG/DC_202012_0383_CrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 5, name: "Spicy Double Chicken Burger", price: 1050.00, image: "ITEM_IMG/DC_202104_0100_DeluxeSpicyCrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 6, name: "Spicy Burger", price: 250.00, image: "ITEM_IMG/DC_202012_0116_SpicyCrispyChicken_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 7, name: "Crispy Chicken Burger", price: 850.00, image: "ITEM_IMG/DC_202012_0383_CrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 8, name: "Spicy Double Chicken Burger", price: 1050.00, image: "ITEM_IMG/DC_202104_0100_DeluxeSpicyCrispyChickenSandwich_PotatoBun_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 9, name: "Co-COla", price: 150.00, image: "ITEM_IMG/DC_202402_0521_MediumCoke_ContourGlassv1_1564x1564_nutrition-calculator-tile.jpeg" },
+    { id: 10, name: "Sprite", price: 200.00, image: "ITEM_IMG/DC_202212_0721_MediumSprite_Glass_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 11, name: "Dr Pepper", price: 180.00, image: "ITEM_IMG/DC_201905_0421_MediumDrPepper_Glass_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 12, name: "Diet Coke", price: 150.00, image: "ITEM_IMG/DC_202112_0652_MediumDietCoke_Glass_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 13, name: "Fanta Orange", price: 180.00, image: "ITEM_IMG/DC_202212_1262_MediumFantaOrange_Glass_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 14, name: "Orange Lavaburst", price: 220.00, image: "ITEM_IMG/DC_202012_0621_MediumHi-COrange_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 15, name: "Milkshake", price: 300.00, image: "ITEM_IMG/DC_201907_1509_MediumChocolateShake_Glass_A1_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 16, name: "MOS Water", price: 80.00, image: "ITEM_IMG/DC_202402_5474_DasaniBottledWater_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 17, name: "French Fries", price: 200.00, image: "ITEM_IMG/DC_202002_6050_SmallFrenchFries_Standing_1564x1564-1_nutrition-calculator-tile.jpeg" },
+    { id: 18, name: "Latte", price: 300.00, image: "ITEM_IMG/DC_201906_7659_MediumIcedMocha_Glass_A1_832x472_nutrition-calculator-tile.jpeg" },
+
+
+
 ];
 
 let orderItems = [];
